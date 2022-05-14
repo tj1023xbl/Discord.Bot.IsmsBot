3b73c47b91b2de90956ef3037e1c7470eeabf6de
TJ Price <spartang072@gmail.com>
2022-05-14 15:44:03 -0400

    Fixing project structure correctly include the database project

---
+++ src/Discord.Bot.Database/Models/UserSayings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Discord.Bot.Database
+{
+    public partial class UserSayings
+    {
+        /// <summary>
+        /// The name of the user containing these isms.
+        /// </summary>
+        [Key]
+        public string UserIsm { get; set; }
+        public List<string> Sayings { get; set; }
+
+        public string Test { get; set; }
+    }
+}
---
+++ src/Discord.Bot.Database/UserSayingsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discord.Bot.Database
+{
+    public partial class UserSayingsContext : DbContext
+    {
+        public virtual DbSet<UserSayings> UserSayings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = "userSayings.db" };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+            optionsBuilder.UseSqlite(connection);
+        }
+    }
+}
