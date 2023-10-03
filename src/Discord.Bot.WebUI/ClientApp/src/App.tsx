f68bdcb90ecbec326604c8cac141bd2a1d287261
Tyler Dalbora <you@example.com>
2023-10-02 21:28:01 -0400

    Initial React commit Display text on the page React app is working

---
+++ src/Discord.Bot.WebUI/ClientApp/src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.scss';
+
+/**
+ * Homepage
+ * @returns
+ */
+export default function App(){ 
+    return (
+        <div className={styles.container}>
+            <h1 className={ styles.intro }>Isms Bot</h1>
+        </div>
+    )
+}
+
---
+++ src/Discord.Bot.WebUI/Controllers/IsmsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Discord.Bot.WebUI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class IsmsController : Controller
+    {
+        /* CRUD */
+
+        // GET
+        
+        // PUT
+
+        // POST
+
+        // DELETE
+    }
+}
