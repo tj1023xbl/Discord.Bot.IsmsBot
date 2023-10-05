1855d9ead1352ab1f11e0e46b2f75533ed1df1a8
Tyler Dalbora <you@example.com>
2023-10-04 20:52:24 -0400

    Add filter to table Filter outer border set to yellow

--- src/Discord.Bot.WebUI/ClientApp/src/components/IsmTable.tsx
+++ src/Discord.Bot.WebUI/ClientApp/src/components/IsmTable.tsx
@@ -1,7 +1,8 @@
-import { createTheme, Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow, tableRowClasses } from "@mui/material"
+import { createTheme, Paper, Table, TableBody, TableCell, Input, TableContainer, TableHead, TableRow, FormControl, InputLabel, TextField, textFieldClasses, InputAdornment, typographyClasses, OutlinedInput, outlinedInputClasses } from "@mui/material"
 import styleVariables from '../variables.module.scss'
 import Saying from "../data/models/Saying"
 import { ThemeProvider } from "@emotion/react";
+import { useCallback, useState } from "react";
 
 
 type Order = 'asc' | 'desc';
@@ -29,6 +30,15 @@ function getComparator<Key extends keyof any>(
 
 const customTheme = createTheme({
     components: {
+        MuiTypography: {
+            styleOverrides: {
+                root: {
+                    [`&.${typographyClasses.root}`]: {
+                        color: 'whitesmoke'
+                    }
+                },
+            }
+        },
         MuiTableRow: {
             styleOverrides: {
                 head: {
@@ -52,23 +62,63 @@ const customTheme = createTheme({
         MuiTableCell: {
             styleOverrides: {
                 root: {
-                    color: `whitesmoke`
+                    color: `whitesmoke`,
+                    border: 'unset !important',
                 },
                 head: {
                     color: `${styleVariables.yellow_dark} !important`,
+                }
+            }
+        },
+        MuiFormLabel: {
+            styleOverrides: {
+                root: {
+                    color: 'whitesmoke !important'
+                }
+            }
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    [`.${outlinedInputClasses.focused}`]: {
+                        borderColor: `${styleVariables.yellow_dark}  !important`,
+                        color: `${styleVariables.yellow_dark}  !important`,
+                    }
+                },
+                input: {
+                    color: 'white',
+                },
+            }
+        },
+        MuiInputBase: {
+            styleOverrides: {
+                root: {
+                    [`&.${outlinedInputClasses.focused} .${outlinedInputClasses.notchedOutline}`]: {
+                        borderColor: `${styleVariables.yellow_light}  !important`,
+                        color: `${styleVariables.yellow_dark}  !important`,
+                    }
                 }
             }
         }
     }
 })
 
+console.log(`&.${outlinedInputClasses.focused} ${outlinedInputClasses.notchedOutline}`)
+
 export const IsmTable = ({ sayings }: { sayings: Saying[] }) => {
 
+    const [filter, setFilter] = useState('');
 
+    const handleFilter = useCallback((saying: Saying) => {
+        return saying.ismKey.includes(filter) ||
+            saying.ismRecorder.includes(filter) ||
+            saying.ismSaying.includes(filter)
+    }, [filter])
 
     return (
         <>
             <ThemeProvider theme={customTheme}>
+                <TextField onChange={(e) => { setFilter(e.target.value) }} value={filter} label='Filter' variant='outlined' InputProps={{ endAdornment: <InputAdornment sx={{ color: 'white' }} position="end">$</InputAdornment> }} />
                 <TableContainer component={Paper}>
                     <Table>
                         <TableHead>
@@ -80,7 +130,7 @@ export const IsmTable = ({ sayings }: { sayings: Saying[] }) => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {sayings.map((saying) => {
+                            {sayings.filter(handleFilter).map((saying) => {
                                 return (
                                     <TableRow key={saying.id}>
                                         <TableCell>{saying.ismSaying}</TableCell>
